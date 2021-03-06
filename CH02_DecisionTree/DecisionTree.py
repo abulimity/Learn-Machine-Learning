91294d3067800390bc974c073383ea92aa438939
abulimity-work <18082895096@163.com>
2021-03-06 20:47:05 +0800

    完成决策时ID3算法实现

--- CH02_DecisionTree/DecisionTree.py
+++ CH02_DecisionTree/DecisionTree.py
@@ -1,5 +1,8 @@
 import numpy as np
+import pandas as pd
 from math import log
+import datetime
+from functools import wraps
 
 
 def createDataSet():
@@ -23,32 +26,51 @@ def createDataSet():
         ]
     )
     # columns = ["年龄", "有工作", "有自己的房子", "信贷情况"]  # 特征标签
-    labels = [
-        0,
-        0,
-        1,
-        1,
-        0,
-        0,
-        0,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        0,
-    ]
+    labels = np.array(
+        [
+            0,
+            0,
+            1,
+            1,
+            0,
+            0,
+            0,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            0,
+        ]
+    )
+    # dataLableMat = np.insert(dataSet, 4, labels, axis=1)
+    dataSet = pd.DataFrame(dataSet, columns=["age", "work", "house", "credit"])
+    labelSet = pd.Series(labels, name="labels")
+    return dataSet, labelSet
 
-    return dataSet, labels
+
+def timing(func):
+    wraps(func)
+
+    def timiingDec(*args, **kwargs):
+        timing = 0.0
+        startTime = datetime.datetime.now()
+        func(*args, **kwargs)
+        endTime = datetime.datetime.now()
+        timing = (endTime - startTime).seconds
+        print("use time: %.2f" % timing)
+
+    return timiingDec
 
 
 def getEnt(labels):
-    classifyCnt = {}
+    # classifyCnt = {}
     labelNum = len(labels)
-    for i in labels:
-        classifyCnt[i] = classifyCnt.get(i, 0) + 1
+    classifyCnt = labels.value_counts()
+    # for i in labels:
+    #     classifyCnt[i] = classifyCnt.get(i, 0) + 1
     ent = 0.0
     for key, cnt in classifyCnt.items():
         p = cnt / labelNum
@@ -56,19 +78,59 @@ def getEnt(labels):
     return ent
 
 
-def getConditionEnt(dataSet, labels):
-    pass
+def maxInfoGain(dataSet, labelSet):
+    rowNum = dataSet.shape[0]
+    ent = getEnt(labelSet)
+
+    bestFeature = {"name": "", "infoGain": 0.0}
+    for feature in dataSet.columns:
+        # print("Now Feature: %s" % feature)
+        colData = dataSet[feature]
+        colValCnt = colData.value_counts().to_dict()
+        conEnt = 0.0
+        for key, val in colValCnt.items():
+            p = val / rowNum
+            # print("Now unique value: %s" % key)
+            conRow = colData[colData == key]
+            conLabel = labelSet.loc[list(conRow.index)]
+            con_ent = p * getEnt(conLabel)
+            # print(con_ent)
+            conEnt += con_ent
+        infoGain = ent - conEnt
+        print("Feature %s ,infoGain %f" % (feature, infoGain))
+        if infoGain > bestFeature["infoGain"]:
+            bestFeature["infoGain"] = infoGain
+            bestFeature["name"] = feature
+    return bestFeature
 
 
-def infoGain(dataSet, feature):
-    pass
+def updateTree(dataSet, sigma):
+    labelDict = dataSet["label"].value_counts().to_dict()
+    featureNum = len(dataSet.columns) - 1
+    tree = {}
+    if len(labelDict) == 1:
+        tree = {"label": list(labelDict.keys())[0]}
+        return list(labelDict.keys())[0]
+    elif featureNum == 0:
+        tree["label"] = sorted(labelDict.items(), key=lambda x: x[1], reverse=True)[0][0]
+        return sorted(labelDict.items(), key=lambda x: x[1], reverse=True)[0][0]
+    bestFeature = maxInfoGain(dataSet.drop("label", axis=1), dataSet.label)
+    if bestFeature["infoGain"] < sigma:
+        tree["label"] = sorted(labelDict.items(), key=lambda x: x[1], reverse=True)[0][0]
+        return sorted(labelDict.items(), key=lambda x: x[1], reverse=True)[0][0]
+    else:
+        bestFeatName = bestFeature["name"]
+        Tree = {bestFeatName: {}}
+        for val in list(dataSet[bestFeatName].value_counts().index):
+            subDataSet = dataSet[dataSet[bestFeatName] == val].drop(bestFeatName, axis=1)
+            Tree[bestFeatName][val] = updateTree(subDataSet, sigma)
+    return Tree
 
 
 if __name__ == "__main__":
-    dataSet, labels = createDataSet()
-    a = np.vstack((dataSet, labels))
+    dataSet, labelSet = createDataSet()
+    dataSet["label"] = labelSet
+    sigma = 0.0
     print(dataSet)
-    print(a)
-
-    # ent = getEnt(labels)
-    # print(ent)
+    Tree = updateTree(dataSet, sigma)
+    print(Tree)
